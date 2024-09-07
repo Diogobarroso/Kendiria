002b8e82666bc08ef263b2a757784c7a4fb7d83c
Diogo Barroso <diogo.barroso@hugintech.com>
2024-09-07 09:54:21 +0100

    added controller movement

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Character : MonoBehaviour
 {
     Rigidbody2D body;
 
+    /*
     float horizontal;
     float vertical;
 
-    Vector2 lookAtPos;
+    public UnityEvent<Vector2> leftStick;
+    public UnityEvent<Vector2> rightStick;
 
-    public float runSpeed = 1.0f;
+    Vector2 lookAtPos;
+    */
+
+    private Vector2 moveDirection = Vector2.zero;
+    private Vector2 inputVector = Vector2.zero;
+
+    public float moveSpeed = 1.0f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -25,16 +34,32 @@ public class Character : MonoBehaviour
 
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
+        //horizontal = Input.GetAxisRaw("Horizontal");
+        //vertical = Input.GetAxisRaw("Vertical");
 
-        lookAtPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //lookAtPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        //body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
 
-        transform.LookAt(lookAtPos,transform.up);
+        //transform.LookAt(lookAtPos, transform.up);
+
+        moveDirection = transform.TransformDirection(inputVector);
+        moveDirection *= moveSpeed;
+        body.velocity = moveDirection;
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        inputVector = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        Vector2 lookAtPos = context.ReadValue<Vector2>();
+        float dir = Mathf.Atan2(lookAtPos.y, lookAtPos.x);
+        transform.rotation = Quaternion.Euler(0, 0, dir * Mathf.Rad2Deg);
     }
 }
