892a0c47cc97cf59a750f27446a16f4c1cc0dd9d
migueldiasbrito <migueldiasbrito@gmail.com>
2024-09-07 12:05:45 +0100

    I hope I didn't fucked up

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class LevelManager : MonoBehaviour
 {
@@ -7,14 +9,18 @@ public class LevelManager : MonoBehaviour
     // TODO: Fire "waves"?
     [SerializeField] private Fire _fire;
 
+    private List<Character> _players = new List<Character>();
+
     private void Start()
     {
         _playerManager.PlayerJoined += OnPlayerJoined;
     }
 
-    private void OnPlayerJoined()
+    private void OnPlayerJoined(Character character)
     {
         _fire.gameObject.SetActive(true);
         _joinInstructions.SetActive(false);
+
+        _players.Add(character);
     }
 }
