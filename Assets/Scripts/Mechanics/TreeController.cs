775b7164b2cf25809bf8656542db4aa1b07617d3
migueldiasbrito <migueldiasbrito@gmail.com>
2024-09-07 15:52:55 +0100

    Burn the tweak

--- Assets/Scripts/Mechanics/TreeController.cs
+++ Assets/Scripts/Mechanics/TreeController.cs
@@ -2,4 +2,51 @@ using UnityEngine;
 
 public class TreeController : MonoBehaviour
 {
+    [SerializeField] private float _maxHealth = 10.0f;
+    [SerializeField] private float _burningDamage = 1.0f;
+    [SerializeField] private LayerMask _fireLayerMask;
+    [SerializeField] private Collider2D _collider;
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    
+    private float _currentHealth = 0.0f;
+    private bool _isBurning = false;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    private void Update()
+    {
+        if (!_isBurning)
+            return;
+
+        _currentHealth -= _burningDamage * Time.deltaTime;
+
+        if (_currentHealth <= 0.0f)
+        {
+            _collider.enabled = false;
+            _spriteRenderer.color = Color.black;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (_isBurning) return;
+        
+        if ((_fireLayerMask & (1 << other.gameObject.layer)) != 0)
+        {
+            _isBurning = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!_isBurning) return;
+        
+        if ((_fireLayerMask & (1 << other.gameObject.layer)) != 0)
+        {
+            _isBurning = false;
+        }
+    }
 }
