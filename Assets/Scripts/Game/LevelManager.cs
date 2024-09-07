9bdafeba9604bb901f4f76d55e1f49ff8b4bd205
Diogo Barroso <diogo.barroso@hugintech.com>
2024-09-07 13:36:13 +0100

    Added wave structures, UI, basic starting waves
    # Conflicts:
    #	Assets/Scripts/Game/LevelManager.cs

--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -9,15 +10,21 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private PlayerManager _playerManager;
     [SerializeField] private GameObject _joinInstructions;
     [SerializeField] private GameObject _gameOverScreen;
+    [SerializeField] private TextMeshProUGUI _waveCounter;
     // TODO: Fire "waves"?
     [SerializeField] private Fire _fire;
 
     private List<Character> _players = new List<Character>();
     [SerializeField] private List<Color> playerColors;
 
+    [SerializeField] private List<Wave> waves;
+    private int currentWave = 0;
+
     private void Start()
     {
         _playerManager.PlayerJoined += OnPlayerJoined;
+
+        _waveCounter.text = 1 + "/" + waves.Count;
     }
 
     private void OnPlayerJoined(Character character)
@@ -50,5 +57,12 @@ public class LevelManager : MonoBehaviour
     public void GoToMainMenu()
     {
         SceneManager.LoadScene(0);
+    }
+    
+    //Connect sth here? Maybe Fire?
+    public void OnFireExtinguished()
+    {
+        _waveCounter.text = currentWave+1 + "/" + waves.Count;
+        //Load new wave
     }
 }
