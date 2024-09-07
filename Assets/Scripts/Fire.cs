ff9245659f84263fc0ebfe7d62a738a33f3c2e7a
Miguel Mendes <miguelgamendes@gmail.com>
2024-09-07 08:59:17 +0200

    Added fire spread. Adapted hose playground to new flame prefab.

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -4,19 +4,67 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour
 {
+    [SerializeField] private GameObject flamePrefab;
+    [SerializeField] private float flameSpreadSpeed;
+    [SerializeField] private float flameSpreadDistance;
     [SerializeField] private float flameAnimSpeed;
     [SerializeField] private float flameAnimHeight;
-    [SerializeField] public Transform[] Flames;
 
-    private float time = 0.0f;
+    private List<Transform> flames = new List<Transform>();
+    private List<float> flameAnimOffset = new List<float>();
+    private List<Vector2> flameBasePositions = new List<Vector2>();
+
+    private float animTime = 0.0f;
+    private float spreadTime = 0.0f;
+
+    private void Start()
+    {
+        // Fire is always composed of a single flame, regardless
+        flames.Add(Instantiate(flamePrefab, this.transform).transform);
+        flameAnimOffset.Add(Random.Range(0.0f, 10.0f));
+        flameBasePositions.Add(Vector2.zero);
+    }
 
     private void Update()
     {
-        time += Time.deltaTime;
+        animTime += Time.deltaTime;
+        spreadTime += Time.deltaTime * flameSpreadSpeed;
 
-        foreach (Transform flame in Flames)
+        // Flame animation
+        for (int i = 0; i < flames.Count; i++)
         {
-            flame.position = new Vector2(0.0f, Mathf.Abs(flameAnimHeight * Mathf.Sin(flameAnimSpeed * time)));
+            flames[i].position = new Vector2(flameBasePositions[i].x, flameBasePositions[i].y + Mathf.Abs(flameAnimHeight * Mathf.Sin(flameAnimSpeed * (animTime + flameAnimOffset[i]))));
+        }
+
+        // Flame spread
+        if (spreadTime >= 1.0f)
+        {
+            List<Transform> newFlames = new List<Transform>();
+            List<float> newFlameAnimOffsets = new List<float>();
+            List<Vector2> newFlameBasePositions = new List<Vector2>();
+
+            foreach (Transform flame in flames) // Check if we can spread each flame
+            {
+                float initialAngle = Random.Range(0.0f, 70.0f);
+                for (float angle = initialAngle; angle < 360.0f + initialAngle; angle += 360.0f/5) // Check an even spacing around the flame
+                {
+                    Vector3 newFlamePosition = flame.position + Quaternion.AngleAxis(angle, Vector3.back) * Vector3.up * flameSpreadDistance;
+                    LayerMask fireLayerMask = LayerMask.GetMask("Fire");
+                    RaycastHit2D hitInfo = Physics2D.CircleCast(newFlamePosition, 0.5f, Vector2.zero, Mathf.Infinity, fireLayerMask);
+                    if (hitInfo.collider == null)
+                    {
+                        newFlames.Add(Instantiate(flamePrefab, newFlamePosition, Quaternion.identity, this.transform).transform);
+                        newFlameAnimOffsets.Add(Random.Range(0.0f, 10.0f));
+                        newFlameBasePositions.Add(newFlamePosition);
+                    }
+                }
+            }
+
+            flames.AddRange(newFlames);
+            flameAnimOffset.AddRange(newFlameAnimOffsets);
+            flameBasePositions.AddRange(newFlameBasePositions);
+
+            spreadTime = 0.0f;
         }
     }
 }
--- Assets/Scripts/Mechanics/WaterController.cs
+++ Assets/Scripts/Mechanics/WaterController.cs
@@ -4,10 +4,6 @@ public class WaterController : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // TODO is this even right script?
-        if (other.TryGetComponent(out Fire fire))
-        {
-            Destroy(fire.gameObject);
-        }
+        Destroy(other.gameObject);
     }
 }
