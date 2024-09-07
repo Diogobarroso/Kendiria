49a9da8ee421845b9a62ca659dbf8f3aeaf9ee88
Diogo Barroso <diogo.barroso@hugintech.com>
2024-09-07 11:27:44 +0100

    added command to hose

--- Assets/Scripts/Hose.cs
+++ Assets/Scripts/Hose.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Hose : MonoBehaviour
 {
@@ -8,20 +9,38 @@ public class Hose : MonoBehaviour
     [SerializeField] private float hosingSpeed;
 
     private float hosingTime = 0.0f;
+    private float timeSinceLastHose = 0.0f;
+
+    [SerializeField] private InputActionAsset controls;
+
+    private void Awake()
+    {
+        controls.FindActionMap("Player").FindAction("Fire").performed += ctx => HoseBeforeHoes();
+    }
 
     void Update()
     {
+        timeSinceLastHose += Time.deltaTime;
+        /*
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            hosingTime += Time.deltaTime;
-            if (hosingTime > 1 / hosingSpeed)
-            {
-                Instantiate(water, this.transform);
-            }
-        }
-        else
+            HoseBeforeHoes();
+        }*/
+    }
+    
+    public void HoseBeforeHoes()
+    {
+        Debug.Log("WHERE MY HOES AT");
+        if (timeSinceLastHose > 0.5f)
         {
             hosingTime = 0.0f;
+        }
+        hosingTime += Time.deltaTime;
+        timeSinceLastHose = 0.0f;
+
+        if (hosingTime > 1 / hosingSpeed)
+        {
+            Instantiate(water, this.transform);
         }
     }
 }
