bbc7c211938dd15701a14e347464f4f24769ba3b
migueldiasbrito <migueldiasbrito@gmail.com>
2024-09-08 14:11:13 +0100

    Add Water Asset

--- Assets/Scripts/Mechanics/WaterController.cs
+++ Assets/Scripts/Mechanics/WaterController.cs
@@ -6,18 +6,9 @@ public class WaterController : MonoBehaviour
     [HideInInspector] public Vector3 baseSpeed;
     [HideInInspector] public Fire fire;
     [SerializeField] private float waterSpeed;
-    [SerializeField] private float lifeTime;
-
-    private float time = 0.0f;
 
     private void Update()
     {
-        time += Time.deltaTime;
-        if (time > lifeTime)
-        {
-            Destroy(gameObject);
-        }
-
         transform.position += baseSpeed * Time.deltaTime + direction * waterSpeed * Time.deltaTime;
     }
 }
