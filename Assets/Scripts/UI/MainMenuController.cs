971e1b69b7fd511d3101fa1d043f2a3dd19a6296
Diogo Barroso <diogo.barroso@hugintech.com>
2024-09-08 11:21:29 +0100

    added controls to main menu

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -6,12 +6,15 @@ using UnityEngine.UI;
 public class MainMenuController : MonoBehaviour
 {
     [SerializeField] private Button _startButton;
+    [SerializeField] private Button _controlsButton;
     [SerializeField] private Button _creditsButton;
     [SerializeField] private Button _quitButton;
     [SerializeField] private Button _backButton;
+    [SerializeField] private Button _controlsBackButton;
 
     [SerializeField] private GameObject _mainMenu;
     [SerializeField] private GameObject _credits;
+    [SerializeField] private GameObject _controls;
 
     [SerializeField] private string _gameSceneName = "MainGameScene";
 
@@ -21,11 +24,14 @@ public class MainMenuController : MonoBehaviour
         _creditsButton.onClick.AddListener(ShowCredits);
         _quitButton.onClick.AddListener(QuitGame);
         _backButton.onClick.AddListener(ShowMainMenu);
+        _controlsButton.onClick.AddListener(ShowControls);
+        _controlsBackButton.onClick.AddListener(ShowMainMenu);
     }
 
     private void ShowMainMenu()
     {
         _credits.SetActive(false);
+        _controls.SetActive(false);
         _mainMenu.SetActive(true);
     }
 
@@ -37,6 +43,14 @@ public class MainMenuController : MonoBehaviour
     private void ShowCredits()
     {
         _credits.SetActive(true);
+        _controls.SetActive(false);
+        _mainMenu.SetActive(false);
+    }
+
+    private void ShowControls()
+    {
+        _credits.SetActive(false);
+        _controls.SetActive(true);
         _mainMenu.SetActive(false);
     }
 
